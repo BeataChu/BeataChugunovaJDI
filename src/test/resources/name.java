aee43b340183a743132ef1e6509b5fdce40676d8
Beata Chu <beataslav@gmail.com>
2019-07-15 20:45:08 +0300

    Minor corrections
    Actualized Pom-file and moved data provider class to the main folder

---
+++ src/test/resources/name.java
@@ -0,0 +1,9 @@
+<!DOCTYPE suite SYSTEM "http://testng.org/testng-1.0.dtd">
+<suite name="Homework 8 - JSON for Metals And Colors Page">
+
+    <test name="JsonProviderForMetalsAndColorsFormTest">
+        <classes>
+            <class name="homework8.DataProviderMetalsColorsTest"/>
+        </classes>
+    </test>
+</suite>
